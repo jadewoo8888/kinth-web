489264c1acc68793cfd6012cab0494012bdb70aa
wuzhy <313388925@qq.com>
2016-05-03 15:35:03 +0800

    添加注释

--- kinth-web/src/com/kinth/interceptor/shiro/ShiroRealm.java
+++ kinth-web/src/com/kinth/interceptor/shiro/ShiroRealm.java
@@ -18,6 +18,7 @@ public class ShiroRealm extends AuthorizingRealm {
 	/*
 	 * 登录信息和用户验证信息验证(non-Javadoc)
 	 * @see org.apache.shiro.realm.AuthenticatingRealm#doGetAuthenticationInfo(org.apache.shiro.authc.AuthenticationToken)
+	 * 获取认证信息
 	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
@@ -33,6 +34,7 @@ public class ShiroRealm extends AuthorizingRealm {
 	/*
 	 * 授权查询回调函数, 进行鉴权但缓存中无用户的授权信息时调用,负责在应用程序中决定用户的访问控制的方法(non-Javadoc)
 	 * @see org.apache.shiro.realm.AuthorizingRealm#doGetAuthorizationInfo(org.apache.shiro.subject.PrincipalCollection)
+	 * 获取授权信息
 	 */
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection pc) {
